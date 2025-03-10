fc663a1c4329d240f7948c6fbbb62d668b15ab26
Aleksa Mikic <aleksamikic@Aleksa-MacBook-Pro.local>
2025-03-10 15:28:11 +0100

    small change

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,6 @@
 <template>
-<<<<<<< HEAD
-  <div><Alert /></div>
-=======
   <div>
     <Alert />
     <h1>OVO je template</h1>
   </div>
->>>>>>> 63b86929270b5b3c58ffa93ff22ddfac445a9182
 </template>
