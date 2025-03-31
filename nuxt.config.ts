fbe33a83ce3539bc41cb40a0f24e229e29415538
Aleksa Mikic <aleksamikic@Aleksa-MacBook-Pro.local>
2025-03-31 20:55:03 +0200

    change in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import { resolve } from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: process.env.NODE_ENV !== "production" },
   alias: {
     "@": resolve(__dirname, "."),
   },
