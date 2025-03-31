4a9ebc4c22e27c453d2cdf4e42017dfbd7514f98
Aleksa Mikic <aleksamikic@Aleksa-MacBook-Pro.local>
2025-03-31 18:18:54 +0200

    Fix build and alias issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,9 @@ import { resolve } from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV !== "production" },
   alias: {
-    "@": resolve(__dirname, "/"),
+    "@": resolve(__dirname, "."),
   },
   css: ["~/assets/main.css"],
   postcss: {
